da116961daf91591fd7bfb58ae63ac1b00c35937
mccrush <mccrush2027@gmail.com>
2024-10-18 12:24:56 +0700

    Remove realtime storage file

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth'
 import database from './modules/database'
-//import realtime from './modules/realtime'
 import storage from './modules/storage'
 
 export default createStore({
